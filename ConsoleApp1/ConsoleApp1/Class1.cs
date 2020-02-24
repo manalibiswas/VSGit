0140f07d3ba3d2d7ef1967e763d32ca31adc1bb9
Manali Biswas <manali.biswas@siemens-healthineers.com>
2020-02-24 16:30:15 +0530

    Added Class1

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public int add()
+        {
+            int i = 10;
+            int j = 20;
+
+            return (i + j);
+        }
+    }
+}
